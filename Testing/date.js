1285871ef1235773f5539ea425b776e30dcab864
OLLYDOTDEV <obell154@gmail.com>
2021-02-20 21:50:09 +1300

    loop for every day of the year

--- Testing/date.js
+++ Testing/date.js
@@ -1,11 +1,38 @@
+//https://www.w3resource.com/javascript-exercises/javascript-date-exercise-14.php
+function days_of_a_year(year) 
+{
+   
+  return isLeapYear(year) ? 366 : 365;
+}
+
+function isLeapYear(year) {
+     return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+}
+
+
+
+
+
+
+
+
+
 var date = new Date();
 
 date.getFullYear();
 date.setHours(1);
-date.setMonth(2);
-date.setDate(12);
-console.log("Time: "+date.getFullYear()+"-"+date.getHours()+"-"+date.getDate())
+date.setMonth(0);
+date.setDate(1);
 
+
+for (i = 0; i < days_of_a_year(date.getFullYear()); i++) {
+
+
+
+console.log('' + date)
+console.log("Time: "+date.getFullYear()+"-"+date.getMonth()+"-"+date.getDate())
+date.setDate(date.getDate() + 1);
+}
 
 /*
 new Date().getDate()          // Return the day as a number (1-31)
@@ -18,4 +45,3 @@ new Date().getMonth()         // Return the month (0-11)
 new Date().getSeconds()       // Return the seconds (0-59)
 new Date().getTime()          // Return the time (milliseconds since January 1, 1970)
 */
-
