d83c1ebeeddcc70a5809381c7edf6a5e4bacab8f
OLLYDOTDEV <obell154@gmail.com>
2021-02-23 21:19:41 +1300

    Arrow Fuctions & Template Literals

---
+++ Testing/TemplateLiterals.js
@@ -0,0 +1,8 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+
+
+var less = 'less';
+var more = 'more'
+
+
+console.log(`i have ${less} lines of code`);
---
+++ Testing/arrorw.js
@@ -0,0 +1,11 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+
+var hello;
+
+hello = () => {
+  return "Hello World!";
+}
+
+
+console.log(hello);
